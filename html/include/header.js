9f97140fb00d1b6e15cd47cc8c9d787e714cb908
EunKyoP <xoxo.popeye77@gmail.com>
2024-02-10 18:42:39 +0900

    메인 헤더 수정

--- html/include/header.js
+++ html/include/header.js
@@ -2,7 +2,7 @@ function includeHeader() {
   let str = `
 <header id="header" class="header">
   <div class="inner">
-    <h1 class="h1"><a href="main.html" aria-label="홈페이지 바로가기">하이유엑스컨설팅</a></h1>
+    <h1 class="h1"><a href="main.html" aria-label="홈페이지 바로가기">박은교 포트폴리오</a></h1>
     <div class="right">
       <button type="button" aria-label="메뉴 열기" class="btn-menu">
         <strong>MENU</strong>
@@ -12,7 +12,6 @@ function includeHeader() {
         <ol>
           <li class="menu-li"><a href="about.html">About</a></li>
           <li class="menu-li"><a href="work.html">Work</a></li>
-          <li class="menu-li"><a href="news.html">News</a></li>
           <li class="menu-li"><a href="contact.html">Contact</a></li>
         </ol>
         <a href="https://hiuxc.com/wp/COMPANY%20BRIEF_HIUX.pdf" target="_blank" aria-label="본사소개서 다운로드" class="btn-download">Company Brief</a>
