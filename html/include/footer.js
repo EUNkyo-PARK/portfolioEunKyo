00cfa2c638ee38936e881b674b4141e11ee28e4b
EunKyoP <xoxo.popeye77@gmail.com>
2024-02-25 17:52:29 +0900

    어바웃까지 완

--- html/include/footer.js
+++ html/include/footer.js
@@ -2,26 +2,8 @@ function includeFooter() {
   let str = `
 <footer id="footer" class="footer">
   <div class="inner">
-    <div class="logo" role="img" aria-label="하이유엑스컨설팅"></div>
+    <div class="logo" role="img" aria-label="박은교 포트폴리오"></div>
     <div class="f-menu-list">
-    </div>
-    <div class="f-info-grp">
-      <div class="info-box">
-        <div class="tit">Office</div>
-        <div class="txt">
-          6F E-605, SK V1 Center,<br />
-          11 Dangsan-ro 41-gil,<br />
-          Yeongdeugpo-gu, Seoul
-        </div>
-      </div>
-      <div class="info-box">
-        <div class="tit">Contact</div>
-        <div class="txt">
-          <a href="tel:0226753397">T 02-2675-3397</a>
-          <a href="tel:0226753387">F 02-2675-3387</a>
-          <a href="mailto:jykim@hiuxc.com">E jykim@hiuxc.com</a>
-        </div>
-      </div>
     </div>
     <span class="copyright">&copy; 2024 HiUX Consulting. All Rights Reserved.</span>
   </div>
--- html/include/header.js
+++ html/include/header.js
@@ -14,7 +14,6 @@ function includeHeader() {
           <li class="menu-li"><a href="work.html">Work</a></li>
           <li class="menu-li"><a href="contact.html">Contact</a></li>
         </ol>
-        <a href="https://hiuxc.com/wp/COMPANY%20BRIEF_HIUX.pdf" target="_blank" aria-label="본사소개서 다운로드" class="btn-download">Company Brief</a>
       </nav>
     </div>
   </div>
