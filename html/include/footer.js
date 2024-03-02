64a49d6e797ed7271397ffd34080b7eac22328b7
EunKyoP <xoxo.popeye77@gmail.com>
2024-03-02 23:53:09 +0900

    cms 상세 완료

--- html/include/footer.js
+++ html/include/footer.js
@@ -5,7 +5,7 @@ function includeFooter() {
     <div class="logo" role="img" aria-label="박은교 포트폴리오"></div>
     <div class="f-menu-list">
     </div>
-    <span class="copyright">&copy; 2024 HiUX Consulting. All Rights Reserved.</span>
+    <span class="copyright">&copy; 2024 EunKyo Park. All Rights Reserved.</span>
   </div>
 </footer>
 <div class="scroll-top"><button type="button" class="btn-scroll-top" aria-label="페이지 상단으로 이동"></button></div>`;
